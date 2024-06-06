97aa8d98a603094586d8f0fee00060f4939c2d36
BielLove <166526327+BielLove@users.noreply.github.com>
2024-06-06 11:32:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//01
+
+let gabriel = document.getElementById("gabriel")
+let mandar = document.getElementById("mandar")
+
+function envia()
+{
+    mandar.innerHTML = "ola " + gabriel.value + ", tudo bem?"
+}
+
+//07
+
+var g = 1
+while (g <= 100)
+{
+console.log(g)
+g = g + 1
+}
+
+//08
+
+var s = 0
+while (s <= 50)
+    {
+        console.log(s)
+        s = s + 5
+    }
+
+    //09
+
+    var m = 100
+    while (m >= 0)
+        {
+            console.log(m)
+            m = m - 10
+        }
+
+
+        
